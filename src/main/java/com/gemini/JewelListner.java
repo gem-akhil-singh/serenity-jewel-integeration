9415310d00889fa2cc47a37242a9c3d4736720b9
Akhil Singh Rajput <akhil.rajput@Geminisolutions.com>
2023-04-27 16:05:16 +0530

    Serenity Jewel Plugin Refractored

--- src/main/java/com/gemini/JewelListner.java
+++ src/main/java/com/gemini/JewelListner.java
@@ -4,111 +4,214 @@ package com.gemini;
 import com.gemini.reporting.GemTestReporter;
 import com.gemini.reporting.STATUS;
 import com.gemini.utils.GemJarGlobalVar;
+import com.gemini.utils.GemJarUtils;
 import net.thucydides.core.model.*;
 import net.thucydides.core.steps.ExecutedStepDescription;
 import net.thucydides.core.steps.StepFailure;
 import net.thucydides.core.steps.StepListener;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.*;
 
 import static com.gemini.utils.GemJarUtils.initializeGemJARGlobalVariables;
 
-public class JewelListner implements StepListener {
+public  class JewelListner implements StepListener {
 
-//    @Inject
- // private LogStorage logStorage;
-//    private SuiteStorage suiteStorage;
+    public JewelListner() {}
 
-    public JewelListner() {
-      //  System.out.println("Inisde Jewel Listner  ");
-        //IntegrationInjector.getInjector().injectMembers(this);
-    }
-
-    public void testSuiteStarted(Class<?> storyClass) {
-
-     //   System.out.println("This only comes once ");
-        //System.out.println("THis runs first Tst Suite started with Story class ");
-        //System.out.println("In the test suite started ");
-        // Not used by listener
-    }
-    //Map<String,Integer> featureStore = new HashMap<String,Integer>();
-    //int counter = 0;
-
-    public static String sRunID = UUID.randomUUID().toString();
+    public static  String SRUNID = UUID.randomUUID().toString();
+    private static final Logger logger = LogManager.getLogger(JewelListner.class);
     public static boolean suiteRunFlag = true;
   public static boolean initialiseGemJarGlobalVar = true;
+
+    @Override
+    public void testSuiteStarted(Class<?> aClass) {
+
+    }
+
     public void testSuiteStarted(Story story) {
+        {
+            if(initialiseGemJarGlobalVar){
+                try{
+                    GemJarUtils.initializeGemJARGlobalVariables();
+                }
+                catch (Exception e){
+                    logger.info("Exception In Loading Values");
+                    logger.debug(e.getMessage());
+                }
+                finally {
+                    if(initialiseGemJarGlobalVar)
+                        initialiseGemJarGlobalVar=false;
+                }
+            }}
 
-      //  System.out.println("This runs first testSuite started with only story");
-      {
-          if(initialiseGemJarGlobalVar){
-        try{
-
-           initializeGemJARGlobalVariables();
-            initialiseGemJarGlobalVar=false;
-        }
-        catch (Exception e){
-            if(initialiseGemJarGlobalVar)
-                    System.out.println("Exception in loading values ");
-                initialiseGemJarGlobalVar=false;
-        }}}
-
-
-        GemJarGlobalVar.s_run_id=sRunID;
-        if(suiteRunFlag==true){
-          //  System.out.println("Inside Flag Check");
+        GemJarGlobalVar.s_run_id=SRUNID;
+        if(suiteRunFlag){
             GemTestReporter.startSuite(GemJarGlobalVar.projectName,GemJarGlobalVar.environment,GemJarGlobalVar.s_run_id);
             suiteRunFlag=false;
         }
-      //  GemJarGlobalVar.s_run_id = GemJarGlobalVar.projectName+GemJarGlobalVar.environment+ UUID.randomUUID().toString();
-       // GemJarGlobalVar.s_run_id = GemJarGlobalVar.projectName+"_"+GemJarGlobalVar.environment+"_"+sRunID;
-     //   System.out.println("Count Occurance of Test Suite Started ");
-       // System.out.println("Test Suite Started ");
-        // Not used by listener
+
     }
 
     public void testSuiteFinished() {
-        GemTestReporter.endSuite();
-        System.out.println("Report Published to Jewel");
+        try {
+            GemTestReporter.endSuite();
+        }
+        catch (Exception e) {
+            logger.info("Failed to finish test Suite");
+            logger.debug(e.getMessage());
+        }
     }
 
-    public void testStarted(String description) {
+    @Override
+    public void testStarted(String s) {
 
-      //  System.out.println("Test Started " + description );
-        // Not used by listener
     }
+
 
     public void testStarted(String description, String id) {
-        int separatorIdx = id.indexOf(";");
-        String category = id.substring(0,separatorIdx);
 
-      GemTestReporter.startTestCase(description.toString(),category,false);
-      //  System.out.println("Test Started + ID  " + description );
-        // Not used by listener
+        try{
+            int separatorIdx = id.indexOf(";");
+            String category = id.substring(0,separatorIdx);
+            GemTestReporter.startTestCase(description,category,false);}
+        catch (Exception e){
+            logger.info("Error In Test Started");
+            logger.debug(e.getMessage());
+        }
+
     }
 
     public void testFinished(TestOutcome result) {
 
-        List<TestStep> testSteps = new ArrayList<>();
-        testSteps = result.getTestSteps();
-     //   TestStep test  = new TestStep();
-      //for each test step get the steps
-        for (TestStep testStep : testSteps) {
-           List<TestStep> serenitySteps =  testStep.getChildren();
-           STATUS status = getStatus(testStep);
-            String stepDescription;
-           if(testStep.getException()!=null || !Objects.equals(testStep.getErrorMessage(), ""))
-               stepDescription = testStep.getErrorMessage();
-           else {
-               stepDescription = testStep.getChildren().toString().substring(1, testStep.getChildren().toString().length() - 1);
-           }
-            System.out.println("");
+        try{
+            //for each test step get the steps
+            for (TestStep testStep : result.getTestSteps()) {
 
-            GemTestReporter.addTestStep(testStep.getDescription(),stepDescription ,getStatus(testStep),testStep.getLastScreenshot().getScreenshot().toString());
+                String stepDescription;
+                if(testStep.getException()!=null || !Objects.equals(testStep.getErrorMessage(), ""))
+                    stepDescription = testStep.getErrorMessage();
+                else {
+                    stepDescription = testStep.getChildren().toString().substring(1, testStep.getChildren().toString().length() - 1);
+                }
+                System.out.println("");
 
+                GemTestReporter.addTestStep(testStep.getDescription(),stepDescription ,getStatus(testStep),testStep.getLastScreenshot().getScreenshot().toString());
+
+            }
+            GemTestReporter.endTestCase();}
+        catch (Exception e){
+            logger.info("Error in End Test");
+
+            logger.debug(e.getMessage());
         }
-        GemTestReporter.endTestCase();
       //before Add test Step Codefor Multip Screenshots will be handled
+    }
+
+    @Override
+    public void testRetried() {
+
+    }
+
+    @Override
+    public void stepStarted(ExecutedStepDescription executedStepDescription) {
+
+    }
+
+    @Override
+    public void skippedStepStarted(ExecutedStepDescription executedStepDescription) {
+
+    }
+
+    @Override
+    public void stepFailed(StepFailure stepFailure) {
+
+    }
+
+    @Override
+    public void lastStepFailed(StepFailure stepFailure) {
+
+    }
+
+    @Override
+    public void stepIgnored() {
+
+    }
+
+    @Override
+    public void stepPending() {
+
+    }
+
+    @Override
+    public void stepPending(String s) {
+
+    }
+
+    @Override
+    public void stepFinished() {
+
+    }
+
+    @Override
+    public void testFailed(TestOutcome testOutcome, Throwable throwable) {
+
+    }
+
+    @Override
+    public void testIgnored() {
+
+    }
+
+    @Override
+    public void testSkipped() {
+
+    }
+
+    @Override
+    public void testPending() {
+
+    }
+
+    @Override
+    public void testIsManual() {
+
+    }
+
+    @Override
+    public void notifyScreenChange() {
+
+    }
+
+    @Override
+    public void useExamplesFrom(DataTable dataTable) {
+
+    }
+
+    @Override
+    public void addNewExamplesFrom(DataTable dataTable) {
+
+    }
+
+    @Override
+    public void exampleStarted(Map<String, String> map) {
+
+    }
+
+    @Override
+    public void exampleFinished() {
+
+    }
+
+    @Override
+    public void assumptionViolated(String s) {
+
+    }
+
+    @Override
+    public void testRunFinished() {
+
     }
 
 
@@ -129,106 +232,4 @@ public class JewelListner implements StepListener {
        return STATUS.INFO;
 
     }
-
-    public void testRetried() {
-        // Not used by listener
-    }
-
-    public void stepStarted(ExecutedStepDescription description) {
-    //    System.out.println("Inside Step Started : " + description.getName());
-        // Not used by listener
-    }
-
-    public void skippedStepStarted(ExecutedStepDescription description) {
-        // Not used by listener
-    }
-
-    public void stepFailed(StepFailure failure) {
-      //  System.out.println("Step Failed ");
-    //    System.out.println(failure.getDescription().toString());
-       // harvestDriverLogs();
-    }
-
-    public void lastStepFailed(StepFailure failure) {
-     //   System.out.println("Inside last Step Failure ");
-    //    System.out.println(failure.getDescription().toString());
-        // Not used by listener
-    }
-
-    public void stepIgnored() {
-        // Not used by listener
-    }
-
-    public void stepPending() {
-        // Not used by listener
-    }
-
-    public void stepPending(String message) {
-        // Not used by listener
-    }
-
-    public void stepFinished() {
-
-   //     System.out.println("Inside Step Finsihed");
-       // harvestDriverLogs();
-    }
-
-    public void testFailed(TestOutcome testOutcome, Throwable cause) {
-   //     System.out.println("Inside Test failed");
-     //   System.out.println(testOutcome.toString());
-        // Not used by listener
-    }
-
-    public void testIgnored() {
-        // Not used by listener
-    }
-
-    public void testSkipped() {
-        // Not used by listener
-    }
-
-    public void testPending() {
-        // Not used by listener
-    }
-
-    public void testIsManual() {
-        // Not used by listener
-    }
-
-    public void notifyScreenChange() {
-        // Not used by listener
-    }
-
-    public void useExamplesFrom(DataTable table) {
-        // Not used by listener
-    }
-
-    public void addNewExamplesFrom(DataTable table) {
-        // Not used by listener
-    }
-
-    public void exampleStarted(Map<String, String> data) {
-        // Not used by listener
-    }
-
-    public void exampleFinished() {
-        // Not used by listener
-    }
-
-    public void assumptionViolated(String message) {
-        // Not used by listener
-    }
-
-    public void testRunFinished() {
-     //   System.out.println("Test Run Ended");
-        // Not used by listener
-    }
-
-//    private void harvestDriverLogs() {
-//        boolean harvestLogs = ReportIntegrationConfig.get().harvestSeleniumLogs;
-//        if (harvestLogs && ThucydidesWebDriverSupport.isDriverInstantiated()) {
-//            Logs logs = ThucydidesWebDriverSupport.getDriver().manage().logs();
-//            logStorage.collect(logs);
-//        }
-//    }
 }
